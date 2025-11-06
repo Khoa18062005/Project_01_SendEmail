fd06bb662c2870065fa45ef39f41791849927480
Khoa18062005 <nguyenquockhoa00725005@gmail.com>
2025-11-06 19:54:12 +0700

    first commit

---
+++ src/main/java/UserDB.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class UserDB {
+    public static void insert(User user) {
+        // Tạm thời chỉ in ra console, chưa cần kết nối database
+        System.out.println("Đã lưu user: " + user.getEmail());
+    }
+}
